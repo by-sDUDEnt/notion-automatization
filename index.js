c8e827343f29d04d4f22747efe63ee08f93ed2ed
by-sDUDEnt <ia04kuchynskyiboryslav@gmail.com>
2023-01-03 05:08:32 +0200

    -nest js| +api test

---
+++ index.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+const { Client } = require('@notionhq/client');
+
+const notion = new Client({ auth: process.env.NOTION_API_KEY });
+
+
+
+(async () => {
+  const databaseId = process.env.NOTION_DATABASE_ID;
+  const response = await notion.databases.retrieve({ database_id: databaseId });
+  console.log(response);
+})();
